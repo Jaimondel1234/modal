818e34c96fabbf621b5d68ce6d6ec8a52cf7e915
Jaime <jaimondel1234@gmail.com>
2020-11-20 10:55:52 +0000

    Redux implemented

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ModalApp } from "./ModalApp";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
-ReactDOM.render(<ModalApp />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <ModalApp />
+  </Provider>,
+  document.getElementById("root")
+);
